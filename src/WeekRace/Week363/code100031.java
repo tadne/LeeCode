e0aa126e3d74aa290097473f4fa9322bb9fa07c3
tadne <2229312952@qq.com>
2023-09-17 14:37:05 +0800

    2023年9月17日14:36:16
    今天是周赛,但是很难受,今天这个10点40要开会,只做了一题

---
+++ src/WeekRace/Week363/code100031.java
@@ -0,0 +1,38 @@
+package WeekRace.Week363;
+
+import java.util.*;
+
+public class code100031 {
+    public static void main(String[] args) {
+        //给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。
+        //
+        //请你用整数形式返回 nums 中的特定元素之 和 ，这些特定元素满足：其对应下标的二进制表示中恰存在 k 个置位。
+        //
+        //整数的二进制表示中的 1 就是这个整数的 置位 。
+        //
+        //例如，21 的二进制表示为 10101 ，其中有 3 个置位。
+        int [] arr={5,10,1,5,2};
+        List<Integer> list=new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(1);
+        list.add(5);
+        list.add(2);
+        int a=1;
+        System.out.println(sumIndicesWithKSetBits(list, a));
+
+    }
+    public static int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum=0;
+        for (int i = 0; i < nums.size(); i++) {
+            String s = Integer.toBinaryString(i);
+            int count=0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j)=='1') count++;
+            }
+            if (count==k) sum+=nums.get(i);
+            System.out.println(nums.get(i)+"====="+s+"======"+count);
+        }
+        return sum;
+    }
+}
