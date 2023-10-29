95b96440332fdecd65989fc8a3f01792717f1423
tadne <2229312952@qq.com>
2023-10-29 11:53:52 +0800

    add,合并两个有序链表,新增递归合并,非常优雅

--- src/LeeCode/code21_合并两个有序链表.java
+++ src/LeeCode/code21_合并两个有序链表.java
@@ -33,6 +33,25 @@ public class code21_合并两个有序链表 {
         return dummy.next;
     }
 
+
+
+    //        作者：腐烂的橘子
+//        链接：https://leetcode.cn/problems/merge-two-sorted-lists/solutions/103891/yi-kan-jiu-hui-yi-xie-jiu-fei-xiang-jie-di-gui-by-/
+//        来源：力扣（LeetCode）
+//        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    //递归的极简操作,爱了爱了
+    public static ListNode mergeTwoLists1(ListNode l1,ListNode l2){
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
     private static class ListNode {
         int val;
         ListNode next;
