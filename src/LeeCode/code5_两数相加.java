09c3ff4d312e648c4dfd4146a1c41402df55ea32
tadne <2229312952@qq.com>
2024-01-14 13:35:53 +0800

    fix:修改题号

--- src/LeeCode/code5_两数相加.java
+++ src/LeeCode/code5_两数相加.java
@@ -1,6 +1,6 @@
 package LeeCode;
 
-public class code2_两数相加 {
+public class code5_两数相加 {
     public static void main(String[] args) {
         //给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
         //
