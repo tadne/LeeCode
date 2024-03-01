0e6123b0cac778f6c807c0e0a604317f0d3a80d9
tadne <2229312952@qq.com>
2024-03-01 23:09:04 +0800

    review:middle:修理注释

--- src/LeeCode/code421_数组中两个数的最大异或值.java
+++ src/LeeCode/code421_数组中两个数的最大异或值.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class code421_数组中两个数的最大异或值 {
     public static void main(String[] args) {
         //给你一个整数数组 nums ，返回 nums[i] XOR nums[j] 的最大运算结果，其中 0 ≤ i ≤ j < n 。
-        System.out.println(findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8}));
+        System.out.println(findMaximumXOR(new int[]{3, 10, 7, 27, 2, 8}));
     }
     // 1+1=0
     // 1+0=1
@@ -15,20 +15,20 @@ public class code421_数组中两个数的最大异或值 {
         int max = Arrays.stream(nums).max().getAsInt();
         int high = 31 - Integer.numberOfLeadingZeros(max);//获取最大值的前置1的位置
 
-        int ans = 0, mask = 0;
-        Set<Integer> seen = new HashSet<>();//哈希表，用来保存重复的是0还是1的
-        for (int i = high; i >= 0; i--) { // 从最高位开始枚举
-            seen.clear();
-            mask |= 1 << i;
-            int newAns = ans | (1 << i); // newAns让ans的第i位为1，保存可以为1的位
-            for (int x : nums) {
-                x &= mask; // 低于 i 的比特位置设置为 0，以及 高于i的位中之前验证的不可能为1的位设置为0
-                if (seen.contains(newAns ^ x)) {//如果集合中存在说明这个比特位可以为1，因为mask保证了x高于i的位一定是满足之前位的条件
-                    ans = newAns; // 这个比特位可以是 1
+        int ans=0,mask=0;//mask起到了记录的作用，不停的记录上次循环中ans
+        Set<Integer> set=new HashSet<>();//用来记录保存的重复的值是0还是1
+        for (int i = high; i >=0; i--) {
+            set.clear();
+            int newAns=ans|(1<<i);//这样可以让newAns保证最大，是希望的结果
+            mask|=(1<<i);  //这其实就是newAns让第i位之后为0的样子
+            for (int num : nums) {
+                num&=mask;//让当前这个num的第i位之后的都变成0，i位之前的必须和mask统一才能是1，这样就保证num的大小小于等于原来
+                if (set.contains(newAns^num)) {//如果有num1和num2，num1^newAns=num2,这样就可以推出，num1^num2=newAns,也就是第i位可以是1
+                    ans=newAns;//刷新ans
                     break;
                 }
-                seen.add(x);
-            }
+                set.add(num);
+            }//如果if语句一直是false，那ans的第i位一定是0，这时newAns的第i位之后都是0，所有不需要调整
         }
         return ans;
 
@@ -38,4 +38,5 @@ public class code421_数组中两个数的最大异或值 {
 //        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
     }
+
 }
